aa95e81f02c45aac18208e63638bd76c45dd3b5d
Sgobbi-R <renato.sgobbi@gmail.com>
2019-04-16 11:39:39 +0200

    aggiornamento titolo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer(function (req, res){
     res.writeHead(200, {'Conntent-Type': 'text/html'});
-    res.write('<h3>Ciao Mondo</h3>');
+    res.write('<h2>Ciao Mondo</h2>');
     res.end();
 
 });
