01ab7d2d4ca7ce7caf1dda3b283cc66b0092aaf1
Sgobbi-R <renato.sgobbi@gmail.com>
2019-04-16 12:36:09 +0200

    safe copy

---
+++ dev.js
@@ -0,0 +1,4 @@
+const http = require('http');
+
+// uso questa variabile che sarà la mia porta
+const port = 8084;
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const http = require('http');
 
+// uso questa variabile che sarà la mia porta
+const port = 8081;
 
+//creo il mio server
 const server = http.createServer(function (req, res){
-    res.writeHead(200, {'Conntent-Type': 'text/html'});
-    res.write('<h2>Ciao Mondo</h2>');
-    res.end();
+    res.write(req.url);
+    if (req.url === '/'){
 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Ciao Mondo</h1>');
+        res.end();
+    }else if (req.url === '/api'){
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(`<h2>${req.url}</h2>`);
+        res.end();
+    }else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(JSON.stringify(
+                {
+                    "version": "0.1",
+                    "nome": "Mia API personale",
+                    "data" : "2019-04-16"
+                }
+            ));
+            res.end();
+        }
 });
 
 // in ascolto
 //server listen (porta,url);
-server.listen(8081, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8081');
+server.listen(port, '127.0.0.1');
+console.log(`Server running at http://127.0.0.1:${port}`);
+
+
+
