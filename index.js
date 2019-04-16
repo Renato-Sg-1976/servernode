fb01f870cba56b9e829039f8fff4cc8e7e227b03
Sgobbi-R <renato.sgobbi@gmail.com>
2019-04-16 12:45:58 +0200

    AGG4

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
+// semplice web server
+// modulo http
 const http = require('http');
+const port = 8082;
 
-// uso questa variabile che sarà la mia porta
-const port = 8081;
 
-//creo il mio server
-const server = http.createServer(function (req, res){
-    res.write(req.url);
-    if (req.url === '/'){
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>Ciao Mondo</h1>');
-        res.end();
-    }else if (req.url === '/api'){
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(`<h2>${req.url}</h2>`);
-        res.end();
-    }else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(JSON.stringify(
-                {
-                    "version": "0.1",
-                    "nome": "Mia API personale",
-                    "data" : "2019-04-16"
-                }
-            ));
-            res.end();
+// comportamento web server con callback
+const server = http.createServer(function (req, res) {
+  console.log(req.url)
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Home</h1>');
+    res.end();
+  } else if (req.url === '/api') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(JSON.stringify(
+      [
+        {
+          "nome": "Gino",
+          "cognome": "Rossi"
+        },
+        {
+          "nome": "Ken",
+          "cognome": "Norris"
         }
-});
-
+      ]
+    ));
+    res.end();
+  }  else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(`<h1>${req.url}</h1>`);
+    res.end();
+  }
+})
 // in ascolto
-//server listen (porta,url);
+// server.listen(porta, url);
 server.listen(port, '127.0.0.1');
+
 console.log(`Server running at http://127.0.0.1:${port}`);
-
-
-
