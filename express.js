67724a68a8da007ea7e6c566d6b7a29ff1567a36
Sgobbi-R <renato.sgobbi@gmail.com>
2019-04-19 10:05:10 +0200

    node process examples

--- express.js
+++ express.js
@@ -1,10 +1,19 @@
 const express = require('express')
 
+let port = process.argv[3] || 3000
+
 const app = express()
 
+console.log(process.argv)
+
+
+console.log(process.argv[2])
+if (process.argv[2]==='v'){
+  console.log('MyApp versione 0.0.1')
+}
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000)
-console.log(`Server running at http://127.0.0.1:3000`);
+app.listen(port)
+console.log(`Server running at http://127.0.0.1:${port}`);
